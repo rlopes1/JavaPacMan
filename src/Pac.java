d9f4c9e2053477e78b7f33ad80311942b9c5d8ed
rlopes1 <44005400+rlopes1@users.noreply.github.com>
2022-10-11 10:08:08 -0400

    Added animations
    Created looping animations based on the movement direction for pacman

--- src/Pac.java
+++ src/Pac.java
@@ -13,15 +13,16 @@ public class Pac extends JPanel {
     private int pacSize;
     private Game game;
     private int buffer; // used for collision calculations
-    private int animDirection = 0;
-    private double animFrame;
-    private boolean moving = false;
+    private double animFrame = 0;
+    private boolean counter = false;
 
     // Constructor for Pac including current game, start pos, and size of drawing
     public Pac(Game game, int startCol, int startRow, int pacSize) {
         this.game = game;
-        x = startCol * game.getTileSize() + (game.getTileSize()/6);;
-        y = startRow * game.getTileSize() + (game.getTileSize()/6);;
+        x = startCol * game.getTileSize() + (game.getTileSize() / 6);
+        ;
+        y = startRow * game.getTileSize() + (game.getTileSize() / 6);
+        ;
         this.pacSize = pacSize;
         startPos[0] = x;
         startPos[1] = y;
@@ -39,6 +40,7 @@ public class Pac extends JPanel {
 
     // Move method
     public void move() {
+        System.out.println(counter);
         // Game state checking before any movement is run
         if (Life.getLives() == 0) {
             game.gameOver();
@@ -53,50 +55,61 @@ public class Pac extends JPanel {
         pacNode = game.getCurrTile(x, y);
         // System.out.println("row:" + pacNode[0] + ", col:" + pacNode[1]);
         if (yVel == 1) {
-            pacNode = game.getCurrTile(x, y + buffer);
+            pacNode = game.getCurrTile(x, y + 4 * buffer);
+
             if (game.getCurrMaze().getValue(pacNode[0], pacNode[1]) != 1) {
                 y += yVel;
+                animFrame += 0.07;
             } else {
                 yVel = 0;
             }
         } else if (yVel == -1) {
-            pacNode = game.getCurrTile(x, y + buffer);
+            pacNode = game.getCurrTile(x, y - buffer);
+
             if (game.getCurrMaze().getValue(pacNode[0], pacNode[1]) != 1) {
                 y += yVel;
+                animFrame += 0.07;
             } else {
                 yVel = 0;
             }
         } else if (xVel == 1) {
             pacNode = game.getCurrTile(x + buffer, y);
+
             if (game.getCurrMaze().getValue(pacNode[0], pacNode[1]) != 1) {
                 x += xVel;
+                animFrame += 0.07;
             } else {
                 xVel = 0;
             }
         } else if (xVel == -1) {
             pacNode = game.getCurrTile(x - buffer, y);
+
             if (game.getCurrMaze().getValue(pacNode[0], pacNode[1]) == 1) {
                 xVel = 0;
             } else {
                 x += xVel;
+                animFrame += 0.07;
             }
         }
-
     }
 
     // Setters and getters for pacmans position
     public void setX(int x) {
         this.x = x;
     }
+
     public void setY(int y) {
         this.y = y;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
+
     public int[] getPacNode() {
         return pacNode;
     }
@@ -106,32 +119,23 @@ public class Pac extends JPanel {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             xVel = 1;
             yVel = 0;
-            animDirection = 0;
-            moving = true;
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             xVel = -1;
             yVel = 0;
-            animDirection = 8;
-            moving = true;
         } else if (e.getKeyCode() == KeyEvent.VK_UP) {
             yVel = -1;
             xVel = 0;
-            animDirection = 12;
-            moving = true;
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             yVel = 1;
             xVel = 0;
-            animDirection = 4;
-            moving = true;
         }
-        animFrame = animDirection;
     }
 
     // keyReleased removes all speed once keys let go
     public void keyReleased(KeyEvent e) {
         xVel = 0;
         yVel = 0;
-        moving = false;
+        animFrame = 0;
     }
 
     // Rectangle return for ghost hit detection
@@ -142,10 +146,25 @@ public class Pac extends JPanel {
     // Paint of the pacman object
     public void paint(Graphics2D g2d) {
         g2d.setColor(Color.yellow);
-        drawAnim(g2d);
+
+        if (xVel == 1) {
+            drawAnimRight(g2d);
+        } else if (xVel == -1) {
+            drawAnimLeft(g2d);
+        } else if (yVel == 1) {
+            drawAnimDown(g2d);
+        } else if (yVel == -1) {
+            drawAnimUp(g2d);
+        } else {
+            drawAnimRight(g2d);
+        }
+
+        if ((int) animFrame == 3) {
+            animFrame = 0;
+        }
     }
 
-    private void drawAnim(Graphics2D g2d) {
+    private void drawAnimRight(Graphics2D g2d) {
         switch ((int) animFrame) {
             case 0:
                 g2d.fillArc(x, y, pacSize, pacSize, 45, 270); // facing right
@@ -159,61 +178,58 @@ public class Pac extends JPanel {
             case 3:
                 g2d.fillArc(x, y, pacSize, pacSize, 20, 320);
                 break;
-            case 4:
+
+        }
+    }
+
+    private void drawAnimDown(Graphics2D g2d) {
+        switch ((int) animFrame) {
+            case 0:
                 g2d.fillArc(x, y, pacSize, pacSize, 315, 270); // facing down
                 break;
-            case 5:
+            case 1:
                 g2d.fillArc(x, y, pacSize, pacSize, 290, 320);
                 break;
-            case 6:
+            case 2:
                 g2d.fillArc(x, y, pacSize, pacSize, 290, 360);
                 break;
-            case 7:
+            case 3:
                 g2d.fillArc(x, y, pacSize, pacSize, 290, 320);
                 break;
-            case 8:
+        }
+    }
+
+    private void drawAnimLeft(Graphics2D g2d) {
+        switch ((int) animFrame) {
+            case 0:
                 g2d.fillArc(x, y, pacSize, pacSize, 225, 270); // facing left
                 break;
-            case 9:
+            case 1:
                 g2d.fillArc(x, y, pacSize, pacSize, 200, 320);
                 break;
-            case 10:
+            case 2:
                 g2d.fillArc(x, y, pacSize, pacSize, 200, 360);
                 break;
-            case 11:
+            case 3:
                 g2d.fillArc(x, y, pacSize, pacSize, 200, 320);
                 break;
-            case 12:
+        }
+    }
+
+    private void drawAnimUp(Graphics2D g2d) {
+        switch ((int) animFrame) {
+            case 0:
                 g2d.fillArc(x, y, pacSize, pacSize, 135, 270); // facing up
                 break;
-            case 13:
+            case 1:
                 g2d.fillArc(x, y, pacSize, pacSize, 115, 320);
                 break;
-            case 14:
+            case 2:
                 g2d.fillArc(x, y, pacSize, pacSize, 115, 360);
                 break;
-            case 15:
+            case 3:
                 g2d.fillArc(x, y, pacSize, pacSize, 115, 320);
                 break;
-        }
-
-
-        if(moving) {
-            animFrame += 0.1;
-            if ((int) animFrame == 3) {
-                animFrame = 0;
-            }
-            if ((int) animFrame == 7) {
-                animFrame = 4;
-            }
-            if ((int) animFrame == 11) {
-                animFrame = 8;
-            }
-            if ((int) animFrame == 15) {
-                animFrame = 12;
-            }
-        } else {
-            animFrame = animDirection;
         }
     }
 }
